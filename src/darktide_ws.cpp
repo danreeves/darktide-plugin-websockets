945d095e6d236d86e0adfdf277e12955a9678728
Dan Reeves <hey@danreev.es>
2023-12-19 02:09:39 +0000

    ssl

--- src/darktide_ws.cpp
+++ src/darktide_ws.cpp
@@ -1,7 +1,6 @@
 
-#include <Windows.h>
 #include <cpprest/ws_client.h>
-
+#include <Windows.h>
 #include <ctime>
 #include <iostream>
 #include <sstream>
@@ -14,46 +13,26 @@ using namespace web::websockets::client;
 
 #include <iostream>
 #include <websocketpp/client.hpp>
-#include <websocketpp/config/asio_no_tls_client.hpp>
+#include <websocketpp/config/asio_client.hpp>
 
-LuaApi128 *lua{};
-LoggingApi *logger{};
-
-static const char *get_name() { return "Darktide Websockets"; }
-
-// int setup_ws() {
-//	websocket_client client;
-//	client.connect(U("ws://localhost:8000")).then([]() {
-//logger->info(get_name(), "Connected to server..."); }).wait();
-//
-//	websocket_outgoing_message msg;
-//	msg.set_utf8_message("I am a UTF-8 string! (Or close enough...)");
-//	client.send(msg).then([]() {
-//		logger->info(get_name(), "Sent message...");
-//		});
-//
-//	client.receive().then([](websocket_incoming_message msg) {
-//		return msg.extract_string();
-//		}).then([](std::string body) {
-//			logger->info(get_name(), "Recieved message...");
-//			logger->info(get_name(), body.c_str());
-//
-//			}).wait();
-//
-//     return 0;
-// }
-
-typedef websocketpp::client<websocketpp::config::asio_client> client;
+typedef websocketpp::client<websocketpp::config::asio_tls_client> client;
+typedef std::shared_ptr<boost::asio::ssl::context> context_ptr;
 
 using websocketpp::lib::bind;
 using websocketpp::lib::placeholders::_1;
 using websocketpp::lib::placeholders::_2;
 
+LuaApi128 *lua{};
+LoggingApi *logger{};
+
+client c;
+client::connection_ptr con;
+
+static const char *get_name() { return "Darktide Websockets"; }
+
 // pull out the type of messages sent by our config
 typedef websocketpp::config::asio_client::message_type::ptr message_ptr;
 
-// This message handler will be invoked once for each incoming message. It
-// prints the message and then sends a copy of the message back to the server.
 void on_message(client *c, websocketpp::connection_hdl hdl, message_ptr msg) {
   logger->info(get_name(), msg->get_payload().c_str());
 
@@ -65,24 +44,43 @@ void on_message(client *c, websocketpp::connection_hdl hdl, message_ptr msg) {
   }
 }
 
-// Create a client endpoint
-client c;
+static context_ptr on_tls_init() {
+	// establishes a SSL connection
+	context_ptr ctx = std::make_shared<boost::asio::ssl::context>(boost::asio::ssl::context::sslv23);
 
-client::connection_ptr con;
+	try {
+		ctx->set_options(boost::asio::ssl::context::default_workarounds |
+			boost::asio::ssl::context::no_sslv2 |
+			boost::asio::ssl::context::no_sslv3 |
+			boost::asio::ssl::context::single_dh_use);
+	}
+	catch (std::exception& e) {
+		logger->info(get_name(), e.what());
+	}
+	return ctx;
+}
+
+void on_open(websocketpp::connection_hdl hdl) {
+	logger->info(get_name(), "Websocket connection open");
+}
+
+void on_close(websocketpp::connection_hdl hdl) {
+	logger->info(get_name(), "Websocket connection closed");
+}
 
 int setup_ws() {
-  std::string uri = "ws://localhost:8000";
+  std::string uri = "wss://ws.darkti.de";
 
   try {
     // Set logging to be pretty verbose (everything except message payloads)
     c.set_access_channels(websocketpp::log::alevel::all);
     c.clear_access_channels(websocketpp::log::alevel::frame_payload);
 
-    // Initialize ASIO
     c.init_asio();
-
-    // Register our message handler
+	c.set_tls_init_handler(bind(&on_tls_init));
     c.set_message_handler(bind(&on_message, &c, ::_1, ::_2));
+	c.set_open_handler(on_open);
+	c.set_close_handler(on_close);
 
     websocketpp::lib::error_code ec;
     con = c.get_connection(uri, ec);
@@ -92,15 +90,11 @@ int setup_ws() {
     }
 
     // Note that connect here only requests a connection. No network messages
-    // are exchanged until the event loop starts running in the next line.
+    // are exchanged until the event loop starts running 
     c.connect(con);
 
     // c.send(con, "hello", websocketpp::frame::opcode::text, ec);
 
-    // Start the ASIO io_service run loop
-    // this will cause a single connection to be made to the server. c.run()
-    // will exit when this connection is closed.
-    // c.poll();
   } catch (websocketpp::exception const &e) {
     logger->info(get_name(), e.what());
   }
@@ -111,8 +105,6 @@ int setup_ws() {
 static void setup_game(GetApiFunction get_engine_api) {
   lua = (LuaApi128 *)get_engine_api(LUA_API_ID);
   logger = (LoggingApi *)get_engine_api(LOGGING_API_ID);
-
-  logger->info("Darktide Websockets", "aaaaaaaaaa");
 
   setup_ws();
 
